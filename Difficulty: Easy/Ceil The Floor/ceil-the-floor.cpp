e43b6003b92389486ae0a148811de7b6f683cac3
Aastha <aasthanagpal05@gmail.com>
2025-04-01 21:43:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Ceil The Floor/ceil-the-floor.cpp
+++ Difficulty: Easy/Ceil The Floor/ceil-the-floor.cpp
@@ -11,27 +11,20 @@ using namespace std;
 class Solution {
   public:
     vector<int> getFloorAndCeil(int x, vector<int> &arr) {
-        // code here
-        int n=arr.size();
-        int flr=-1,cl=INT_MAX;
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i]==x)
-            {
-                cl=arr[i];
-                flr=arr[i];
-            }
-            else if(arr[i]>x)
-                cl=min(cl,arr[i]);
-            else
-            {
-                flr=max(flr,arr[i]);
-            }
-        }
-        if(cl!=INT_MAX)
-            return {flr,cl};
-        else
-            return {flr,-1};
+        //code here
+       int floor = -1;
+       int ceil = -1;
+       for(int i = 0; i<arr.size(); i++){
+           if(arr[i] <= x){
+               floor = max(floor, arr[i]);
+           }
+           if(arr[i] >= x){
+               if(ceil == -1 || arr[i]< ceil){
+                   ceil = arr[i];
+               }
+           }
+       }
+       return {floor, ceil};
     }
 };
 
